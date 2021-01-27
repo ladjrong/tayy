d4bc7e77b4290a9d55f7908e9e96a16b65fb05b9
djrong <100102888@qq.com>
2021-01-27 09:13:50 +0800

    djrong

---
+++ tayy/__init__.py
@@ -0,0 +1,52 @@
+import socket
+import datetime
+import requests
+import json
+class remoteLog():
+
+    def __init__(self,scriptname=None):
+        self.scriptname = scriptname
+        self.url = 'http://127.0.0.1'
+
+    def set_url(self,url):
+        self.url = url
+
+    def get_ip(self):
+
+        try:
+
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(('8.8.8.8', 80))
+            ip = s.getsockname()[0]
+
+        finally:
+
+            s.close()
+
+        return ip
+
+    def get_scriptname(self):
+
+        return self.scriptname
+    def Script_name(self):
+        a=str(self.get_scriptname()).split("/")
+        return a
+
+    def log(self,url,execution_tb):
+        try:
+            data = {'script_update':datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                    'ip':str(self.get_ip()),'location':self.get_scriptname(),"script_name":self.Script_name()[-1],"execution_time":execution_tb,
+                    }
+            data_a = json.dumps(data)
+            a=requests.post(url, data_a)
+            return data
+        except:
+            print("连接失败")
+def dgta(url_a,execution_time):
+    a = remoteLog(__file__)
+    a.log(url_a,execution_time)
+    return a
+
+
+
+
